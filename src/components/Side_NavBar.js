4feefd0a54f5a6156df0ac83c6cc19325df49737
raccococo <sirio.pastori@gmail.com>
2023-10-30 21:50:28 +0100

    Add some emoji

--- src/components/Side_NavBar.js
+++ src/components/Side_NavBar.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 
 const Side_NavBar = () => {
   return (
-    <div style={{ flexGrow: 0.15, justifyContent: "center", alignItems: "center" }}>
+    <div style={{ flexGrow: 0.15, justifyContent: "center", alignItems: "center", color: "white" }}>
       <table style={{backgroundColor: "#1f2a37", height: "100vh", width: "100%"}}>
         <thead style={{ display: "flex", justifyContent: "center" }}>
-          <tr>
-            <th>Tabella 1</th>
+          <tr style={{marginTop: "40%"}}>
+            <th>CoralMC</th>
           </tr>
         </thead>
         <tbody>
